4b0d43d1aed1ccc8943cd57378c218d640b37a56
wataschi <pivospenkoy@gmail.com>
2022-04-13 02:15:57 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+    "use strict";
+    
+    const typed = new Typed('.prof', {
+        strings: ["Front-End Developer","Web and Mobile"],
+        typeSpeed: 40,
+        backSpeed: 40,
+        backDelay: 2000,
+        startDelay: 1500,
+        loop: true,
+        showCursor: true
+    });
+})(jQuery);
