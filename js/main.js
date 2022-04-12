5284dc2f5028c39254acb43f16418e22d2a033d6
wataschi <pivospenkoy@gmail.com>
2022-04-13 02:44:31 +0300

    main

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+    "use strict";
+
+    const typed = new Typed('.prof', {
+        strings: ["Full stack developer","Web and Mobile"],
+        typeSpeed: 90,
+        backSpeed: 40,
+        backDelay: 2000,
+        startDelay: 1500,
+        loop: true,
+        showCursor: true
+    });
+})(jQuery);
