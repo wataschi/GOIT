0da2b0bce8d9a2b08101d38a6aa8f3859148b632
wataschi <pivospenkoy@gmail.com>
2022-07-20 01:57:42 +0300

    update 20.07

--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
-(function($) {
-    "use strict";
-    const typed = new Typed('.prof', {
-        strings: ["Front-End Developer"],
-        typeSpeed: 40,
-        backSpeed: 40,
-        backDelay: 2000,
-        startDelay: 1500,
-        loop: true,
-        showCursor: true
-    });
-})(jQuery);
+
+inputOneId = 'inputOne'
+inputTwoId = 'inputTwo'
+buttonResId = 'buttonRes'
+outputResId = 'outputRes'
+inputOne= document.getElementById(inputOneId)
+inputTwo= document.getElementById(inputTwoId)
+buttonRes= document.getElementById(buttonResId)
+outputRes= document.getElementById(outputResId)
+
+const inputValue = (e) =>{
+    inputOneValue = inputOne.value;
+    inputTwoValue =inputTwo.value;
+}
+
+const sFn = (e) =>{
+    inputValue();
+    outputRes.value =(Number(inputOneValue)+Number(inputTwoValue))
+}
+const nFn = (e) =>{
+    inputValue();
+    outputRes.value =(Number(inputOneValue)-Number(inputTwoValue))
+}
+const mFn = (e) =>{
+    inputValue();
+    outputRes.value =(Number(inputOneValue)*Number(inputTwoValue))
+}
+const dFn = (e) =>{
+    inputValue();
+    if ( Number(inputTwoValue)===0 || inputOneValue === undefined ){
+        outputRes.value= 'Bad input!'
+    }
+    else{ outputRes.value =(Number(inputOneValue)/Number(inputTwoValue))}
+}
+
+
+
+buttonS.addEventListener("click", sFn);
+buttonN.addEventListener("click", nFn);
+buttonM.addEventListener("click", mFn);
+buttonD.addEventListener("click", dFn);
